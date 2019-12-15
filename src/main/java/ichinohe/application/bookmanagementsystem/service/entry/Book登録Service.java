3658beb9e8e154d7e7d0cf3a39a2d4e8be551d20
renichinohe <renichinohe@icloud.com>
2019-12-16 03:47:52 +0900

    書籍登録サービスを作成

---
+++ src/main/java/ichinohe/application/bookmanagementsystem/service/entry/Book登録Service.java
@@ -0,0 +1,17 @@
+package ichinohe.application.bookmanagementsystem.service.entry;
+
+import ichinohe.application.bookmanagementsystem.domain.entry.BookInfoEntryRepository;
+import ichinohe.application.bookmanagementsystem.domain.entry.書籍登録申込書;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class Book登録Service {
+    @Autowired
+    private BookInfoEntryRepository repository;
+
+    public void entry(書籍登録申込書 application) {
+        repository.apply(application);
+    }
+}
