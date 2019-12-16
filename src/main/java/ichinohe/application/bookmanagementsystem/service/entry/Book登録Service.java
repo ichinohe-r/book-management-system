d53f69223295dd6c77baf177e0f8bb1912c4ec7b
renichinohe <renichinohe@icloud.com>
2019-12-17 05:57:27 +0900

    アノテーションを変更しました

--- src/main/java/ichinohe/application/bookmanagementsystem/service/entry/Book登録Service.java
+++ src/main/java/ichinohe/application/bookmanagementsystem/service/entry/Book登録Service.java
@@ -3,10 +3,9 @@ package ichinohe.application.bookmanagementsystem.service.entry;
 import ichinohe.application.bookmanagementsystem.domain.entry.BookInfoEntryRepository;
 import ichinohe.application.bookmanagementsystem.domain.entry.書籍登録申込書;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class Book登録Service {
     @Autowired
     private BookInfoEntryRepository repository;
