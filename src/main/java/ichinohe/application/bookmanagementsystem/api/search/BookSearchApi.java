951955782b308e629eae5d852fca7febab18ca6b
renichinohe <renichinohe@icloud.com>
2019-12-24 03:45:07 +0900

    不要なコメントを削除しました

--- src/main/java/ichinohe/application/bookmanagementsystem/api/search/BookSearchApi.java
+++ src/main/java/ichinohe/application/bookmanagementsystem/api/search/BookSearchApi.java
@@ -28,13 +28,7 @@ public class BookSearchApi {
     public String apply(BookSearchRequest request, Model model) {
         BookSearchApplication application = request.createApplication();
         List<BookEntity> bookEntityList = service.search(application);
-
-//        List<BookEntity> bookEntityList = new ArrayList<BookEntity>();
-//        bookEntityList.add(new BookEntity(new BookManagementNumber(1) , new Author("Miura"), new BookTitle("Kazuyoshi"), new Publisher("未来")));
-//        bookEntityList.add(new BookEntity(new BookManagementNumber(2) , new Author("Kitazawa"), new BookTitle("Tsuyoshi"), new Publisher("未来")));
-//        bookEntityList.add(new BookEntity(new BookManagementNumber(3) , new Author("Hashiratani"), new BookTitle("Tetsuji"), new Publisher("未来")));
         model.addAttribute("bookEntityList", bookEntityList);
-
         return "search";
     }
 
