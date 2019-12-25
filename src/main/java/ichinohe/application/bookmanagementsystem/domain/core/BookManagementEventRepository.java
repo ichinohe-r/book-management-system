1773da796c15c44d6900a2207a515a0640e52bae
renichinohe <renichinohe@icloud.com>
2019-12-25 09:56:44 +0900

    不要なクラスやコードを削除

--- src/main/java/ichinohe/application/bookmanagementsystem/domain/core/BookManagementEventRepository.java
+++
@@ -1,8 +0,0 @@
-package ichinohe.application.bookmanagementsystem.domain.core;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookManagementEventRepository {
-    void entryEvent();
-}
